1e8750a473041843639de89b6be801c44ff02f5e
Alfred <somethingfly@somethingfly.com>
2022-06-25 15:26:25 -0700

    commit tree 9a1cdd04102998e5fa4e7451e4719803e8553b5c

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(record) {
+    const recordElement = record.find(isW);
+    if (typeof recordElement  !== 'undefined')
+       return recordElement.year;
+    else
+        return recordElement;
+}
+
+function isW(element) {
+    return (element.result === "W");
+}
